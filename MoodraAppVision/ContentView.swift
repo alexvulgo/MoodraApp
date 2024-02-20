d6f0c3cf11c70589068de02b30ac5a7eead92322
alexvulgo <120048791+alexvulgo@users.noreply.github.com>
2024-02-20 12:43:21 +0100

    Changed name info

--- MoodraAppVision/ContentView.swift
+++ MoodraAppVision/ContentView.swift
@@ -22,7 +22,11 @@ struct ContentView: View {
             Model3D(named: "Scene", bundle: realityKitContentBundle)
                 .padding(.bottom, 50)
 
-            Text("Hello, world!")
+            Text("Moodra App")
+                .font(.largeTitle)
+                
+            
+
 
             Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
                 .toggleStyle(.button)
