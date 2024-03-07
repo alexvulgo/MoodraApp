71c9ef44b9b237ad41d380d30fbd23096cc07455
alexvulgo <120048791+alexvulgo@users.noreply.github.com>
2024-03-07 11:22:57 +0100

    fixed onboarding

--- MoodraAppVision/Model/PageModel.swift
+++ MoodraAppVision/Model/PageModel.swift
@@ -21,7 +21,7 @@ struct Page: Identifiable, Equatable {
         Page(name: "Welcome to Moodra!", description: "Moodra makes you meditate using mudras, hand gestures from Indian dances and yoga.", image: "mainIcon", tag: 0),
         Page(name: "Learn Mudras", description: "Select your favourite mudras and put your hands in the same position for five seconds, and the app will tell you if you are performing the position correctly!", image: "Tripataka", tag: 1),
         Page(name: "Meditate", description: "Select three mudras and set a timer to meditate. ", image: "Ardhachandra", tag: 2),
-        Page(name: "A full immersion experience", description: "Choose your favourite relaxing sound and background to enjoy your dynamic meditation!", image: "Main", tag: 3)
+        Page(name: "A full immersion experience", description: "Choose your favourite relaxing sound and background to enjoy your dynamic meditation! Note: You need to be in full immersione mode for use the hand tracking.", image: "Main", tag: 3)
     ]
     
     
